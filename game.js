b52b720781aeb696c2c80e67af5d07d27fc127ba
broggi_t <idee-4@idee-4.(none)>
2014-04-02 11:39:43 +0200

    forked from bitbucket

---
+++ game.js
@@ -0,0 +1,83 @@
+/*
+pieces is an array that represents all the pieces of the game
+
+initPlayer() will create all the pieces for only one player
+initGame() will create all the pieces for all players
+display() will display all the pieces in the map
+isEmpty(x, y) will return a boolean that inform us if the case map[x][y] is empty or not
+*/
+
+map = [8][8];
+pieces = Array();
+
+function initPlayer(color)
+{
+	if (color == noir)
+	{
+		x = 0;
+		pion_x = 1;
+	}
+	else
+	{
+		x = 7;
+		pion_x = 6;
+	}
+	for (i = 0; i < 8; ++i)
+		pieces.push(new Pion(i, pion_x, color));
+	pieces.push(new Tour(0, x, color));
+	pieces.push(new Tour(7, x, color));
+	pieces.push(new Cavalier(1, x, color));
+	pieces.push(new Cavalier(6, x, color));
+	pieces.push(new Fou(2, x, color));
+	pieces.push(new Fou(5, x, color));
+	pieces.push(new Dame(3, x, color));
+	pieces.push(new Roi(4, x, color));
+}
+
+function initGame()
+{
+	initPlayer(blanc);
+	initPlayer(noir);
+}
+
+function displayCoord()
+{
+	tr = document.querySelectorAll("tr")[0];
+	for (i = 0; i < 8; ++i)
+	{
+		tr.querySelectorAll('td')[i + 1].innerHTML = i + 1;
+	}
+	for (i = 0; i < 8; ++i)
+	{
+		tr = document.querySelectorAll("tr")[i + 1];
+		tr.querySelectorAll('td')[0].innerHTML = String.fromCharCode(65 + i);
+	}
+
+}
+
+function display()
+{
+	for (i = 0; i < pieces.length; ++i)
+	{
+		/*
+		that's a tmp test function. it must be rewrited
+
+		pieces images are in the img directory
+		files names are : img/[piece]_[color].png
+		*/
+		x = pieces[i].pos_x + 1;
+		y = pieces[i].pos_y + 1;
+		img = 'url(img/'+pieces[i].piece+'_'+pieces[i].joueur+'.png)';
+		tr = document.querySelectorAll("tr")[y];
+		tr.querySelectorAll('td')[x].style.backgroundImage=img;
+	}
+}
+
+function isEmpty(x, y)
+{
+	;
+}
+
+initGame();
+displayCoord();
+display();
---
+++ pieces.class.js
@@ -0,0 +1,83 @@
+/*
+For each of the following classes :
+
+	joueur attribute is the player, defined by the macros blanc and noir.
+	pos_x and pos_y are the position of the piece.
+	alive is a boolean that tell us if the piece is alive.
+	piece is a string that contains the type of the piece (useful for the map)
+	
+	blocked method return a boolean (the piece can move or note)
+	canMove method return a boolean which tell you if you can move this piece here
+	move method return a boolean (error) and try to move the piece
+*/
+
+var blanc = 'blanc';
+var noir = 'noir';
+function Pion(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'pion';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+	this.played = false;
+
+	this.canMove = function(x, y)
+	{
+
+	};
+	this.blocked = function()
+	{
+
+	};
+	this.move = function(x, y)
+	{
+		if (this.canMove(x, y))
+		{
+			this.pos_x = x;
+			this.pos_y = y;
+			return true;
+		}
+		return false;
+	};
+}
+function Cavalier(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'cavalier';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+}
+function Fou(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'fou';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+}
+function Tour(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'tour';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+}
+function Dame(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'dame';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+}
+function Roi(x, y, joueur)
+{
+	this.joueur = joueur;
+	this.piece = 'roi';
+	this.pos_x = x;
+	this.pos_y = y;
+	this.alive = true;
+}
