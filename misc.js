ebb268b98f6ab2974689004fa8821ab84c967c24
nguye_2 <idee-1@idee-1.(none)>
2014-04-02 16:24:17 +0200

    misc MAJ

--- misc.js
+++ misc.js
@@ -8,18 +8,18 @@ function give_y(number)
 	return (number - 1);
 }
 
-function chain_to_coord(str1, str2)
+function chain_to_coord(str)
 {
-	if ((str1.length != 2) || (str2.length != 2))
+	tab = new Array();
+	if (str.length != 2)
 		alert("Error : Move commad must be like : '[LETTER A - H][NUMBER 1 - 8]'. ");
 	if (!(str[0] >= 'A' && str[0] <= 'H'))
 		alert("Error : Move command must be like : '[LETTER A - H][NUMBER 1 - 8]'. ");
 	if (!(str[1] >= '1' && str[1] <= '8'))
 		alert("Error : Move command must be like : '[LETTER A - H][NUMBER 1 - 8]'. ");
 
-	var x1 = give_x(str1[0]);
-	var y1 = give_y(str1[1]);
-
-	var x2 = give_x(str2[0]);
-	var x2 = give_y(str2[1]);
+	tab[0] = give_x(str[0]);
+	tab[1] = give_y(str[1]);
+	
+	return (tab);
 }
