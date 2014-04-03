ebedec4611d73219517bd29d384f6ef4da85dd4e
Alexandre Tea <alexandre.tea@epitech.eu>
2014-04-03 15:43:07 +0200

    patch click

--- clickevent.js
+++ clickevent.js
@@ -1,3 +1,5 @@
+var status = 0; 
+
 $(document).ready(function(){
     $("#echiquier td").on({
 	mouseenter: function(){
@@ -13,35 +15,42 @@ $(document).ready(function(){
 
     $("#echiquier td").on("click", function(){
 	var id = $(this).attr("id");
-	if (id.length != 2)
-	    return (0);
-	var coo = chain_to_coord(id);
-	if (isEmpty(coo[1], coo[0]) == false)
+	if (status == 0)
 	{
-	    var moves = movePossibs(coo[1], coo[0]);
-	    var i = 0;
-	    var move_id;
-
-	    while (i < moves.length)
+	    if (id.length != 2)
+		return (0);
+	    var coo = chain_to_coord(id);
+	    if (isEmpty(coo[1], coo[0]) == false)
 	    {
-		move_id = coord_to_chain(moves[i][0], moves[i][1]);
-		if (moves[i][2] == true)
+		var moves = movePossibs(coo[1], coo[0]);
+		var i = 0;
+		var move_id;
+
+		while (i < moves.length)
 		{
-		    $("#" + move_id).css("background-color", "blue");
-		    $("#" + move_id).on("click", function(){
-			alert("move");
-			$(this).css("background-color", "white");
-		    });
+		    move_id = coord_to_chain(moves[i][0], moves[i][1]);
+		    if (moves[i][2] == true)
+		    {
+			status = 1;
+			$("#" + move_id).css("background-color", "blue");
+			$("#" + move_id).on("click", function(){
+			    alert("move");
+			    $(this).css("background-color", "white");
+			    status = 0;
+			});
+		    }
+		    else
+		    {
+			status = 1;
+			$("#" + move_id).css("background-color", "red");
+			$("#" + move_id).on("click", function(){
+			    alert("eat");
+			    $("#" + move_id).css("background-color", "white");
+			    status = 0;
+			});
+		    }
+		    i++;
 		}
-		else
-		{
-		    $("#" + move_id).css("background-color", "red");
-		    $("#" + move_id).on("click", function(){
-			alert("eat");
-			$("#" + move_id).css("background-color", "white");
-		    });
-		}
-		i++;
 	    }
 	}
     });
