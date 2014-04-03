1b75a508efbbe1b65fe9c165c5ae6d52d64517f9
nguye_2 <idee-1@idee-1.(none)>
2014-04-03 10:28:43 +0200

    Recup coord fonctionnel

--- misc.js
+++ misc.js
@@ -17,9 +17,9 @@ function chain_to_coord(str)
 	if (str.length != 2)
 		alert("Error : Move commad must be like : '[LETTER A - H][NUMBER 1 - 8]'. ");
 	if (!(str[0] >= 'A' && str[0] <= 'H'))
-		alert("Error : Bad letter [A - H]'");
+		alert("Error : Bad letter : [A - H]");
 	if (!(str[1] >= '1' && str[1] <= '8'))
-		alert("Error : Bad number '[1 - 8]'");
+		alert("Error : Bad number : [1 - 8]");
 
 	tab[0] = give_x(str.charAt(0));
 	tab[1] = give_y(str.charAt(1));
