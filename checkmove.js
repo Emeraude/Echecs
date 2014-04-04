996d5f89974666001f1488c18e91b1da1bffa349
Alexandre Tea <alexandre.tea@epitech.eu>
2014-04-04 15:59:13 +0200

    merge checkmove

--- checkmove.js
+++ checkmove.js
@@ -9,9 +9,14 @@ function	my_no_check(threats)
     {
 	piece = getPiece(threats[i][0], threats[i][1]);
 	if (piece.piece == "tour")
-	    alt = my_tour_alt(alt, piece, king);
+	    alt = my_line_alt(alt, piece, king);
+	else if (piece.piece == "fou")
+	    alt = my_diag_alt(alt, piece, king);
+	else if (piece.piece == "dame")
+	    alt = my_queen_alt(alt, piece, king);
 	i++;
     }
+    return (alt);
 }
 
 function	my_line_alt(alt, piece, king)
@@ -34,6 +39,49 @@ function	my_line_alt(alt, piece, king)
     }
     return (alt);
 }
+
+function    my_diag_alt(alt, piece, king)
+{
+    var possibs = new Array();
+
+    possibs = movePossibs(piece.pos_x, piece.pos_y);
+    if (piece.pos_x < king.pos_x)
+    {
+        if (piece.pos_y < king.pos_y)
+        {
+            for (i = 0; i < possibs.length; ++i)
+                if (possibs[i][0] > piece.pos_x && possibs[i][0] < king.pos_x && 
+		    possibs[i][1] > piece.pos_y && possibs[i][1] < king.pos_y)
+                    alt.push(possibs[i]);
+        }
+        else
+        {
+            for (i = 0; i < possibs.length; ++i)
+                if (possibs[i][0] > piece.pos_x && possibs[i][0] < king.pos_x && 
+		    possibs[i][1] < piece.pos_y && possibs[i][1] > king.pos_y)
+                    alt.push(possibs[i]);
+        }
+    }
+    else
+    {
+        if (piece.pos_y < king.pos_y)
+        {
+            for (i = 0; i < possibs.length; ++i)
+                if (possibs[i][0] < piece.pos_x && possibs[i][0] > king.pos_x && 
+		    possibs[i][1] > piece.pos_y && possibs[i][1] < king.pos_y)
+                    alt.push(possibs[i]);
+        }
+        else
+        {
+            for (i = 0; i < possibs.length; ++i)
+                if (possibs[i][0] < piece.pos_x && possibs[i][0] > king.pos_x && 
+		    possibs[i][1] < piece.pos_y && possibs[i][1] > king.pos_y)
+                    alt.push(possibs[i]);
+        }
+    }
+    return (alt);
+}
+
 
 function	my_queen_alt(alt, queen, king)
 {
